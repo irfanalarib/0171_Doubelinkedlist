7396db4673e9771d670abf7a7aa69ef97eb18bb2
irfanalarib <irfanalarib7@gmail.com>
2025-05-15 11:39:32 +0700

    Tambahkan fungsi untuk menampilkan data dari belakang

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -148,4 +148,32 @@ public:
             i++;
         }
     }
+    void revtraverse()
+    {
+        if (START == NULL)
+        {
+        cout << "\nList is empty" << endl;
+        return;
+        }
+
+        //step 1: Move to last node
+        Node *currentNode = START;
+        int i = 0;
+        while(currentNode->next != NULL)
+        {
+            currentNode = currentNode->next;
+            i++;
+        }
+
+        //step 2: Traverse backward
+        cout << "\nRecords is descending order of roll number are:\n";
+        while (currentNode != NULL)
+        {
+            cout << i + 1 << ". " << currentNode->noMhs << " " << endl;
+
+            //step3 : Move to previous node
+            currentNode = currentNode->prev;
+            i--;
+        }
+    }
 };
