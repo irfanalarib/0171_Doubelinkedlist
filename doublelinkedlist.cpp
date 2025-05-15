5020cad5cb0d70bd321514be32642a930d2842b5
irfanalarib <irfanalarib7@gmail.com>
2025-05-15 11:13:17 +0700

    membuat memori untuk node baru pada addnode

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -27,5 +27,8 @@ public:
         string nm;
         cout << "\nEnter the roll number of the student: ";
         cin >> nim;
+
+        //step 1: Allocated memory for new node
+    Node *newNode = new Node();
     }
 };
